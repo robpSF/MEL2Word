e75a430b9c393d4fd34cfc49061bfb3aa9ff0b70
robpSF <robert@tstoryteller.com>
2024-08-21 21:15:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,28 +49,18 @@ def parse_and_add_run(paragraph, text):
             text = text[next_tag:]
 
 # Function to create a cumulative timing table
-def create_cumulative_timing_table(facilitator_content, start_time):
+def create_cumulative_timing_table(timing_info, start_time):
     cumulative_time = start_time
     table_data = []
 
-    for i, item in enumerate(facilitator_content):
-        if i == 0:
-            # For the first row, use the start time provided by the user
-            table_data.append({
-                'Cumulative Time (D days hh:mm:ss)': format_timedelta(cumulative_time - start_time),
-                'Subject': item.get('subject', ''),
-                'Text': item.get('text', ''),
-                'Inject Timing (s)': item.get('timer_seconds', 0)
-            })
-        else:
-            # For subsequent rows, add the timer_seconds to cumulative time
-            cumulative_time += timedelta(seconds=item.get('timer_seconds', 0))
-            table_data.append({
-                'Cumulative Time (D days hh:mm:ss)': format_timedelta(cumulative_time - start_time),
-                'Subject': item.get('subject', ''),
-                'Text': item.get('text', ''),
-                'Inject Timing (s)': item.get('timer_seconds', 0)
-            })
+    for item in timing_info:
+        cumulative_time += timedelta(seconds=item.get('timer_seconds', 0))
+        table_data.append({
+            'Cumulative Time (D days hh:mm:ss)': format_timedelta(cumulative_time - start_time),
+            'Subject': item.get('subject', ''),
+            'Text': item.get('text', ''),
+            'Inject Timing (s)': item.get('timer_seconds', 0)
+        })
 
     return pd.DataFrame(table_data)
 
